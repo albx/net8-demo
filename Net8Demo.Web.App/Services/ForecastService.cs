fab8923c8e85d5d576d5cbf4b5dda0de832d8946
Alberto Mori <albi.mo87@hotmail.it>
2023-05-20 17:55:55 +0200

    preview4 samples

---
+++ Net8Demo.Web.App/Services/ForecastService.cs
@@ -0,0 +1,14 @@
+﻿using Net8Demo.Web.App.Models;
+
+namespace Net8Demo.Web.App.Services;
+
+public class ForecastService
+{
+    public async Task<IEnumerable<WeatherForecastModel>> GetWeatherForecastsAsync()
+    {
+        await Task.Delay(10000);
+
+        return Enumerable.Range(0, 10)
+            .Select(r => new WeatherForecastModel(DateTime.Now, r, $"Temperature is {r} °C"));
+    }
+}
